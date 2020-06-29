b8e574dba2d6dbc3df8008eb17126cd94ddaf1dd
gabriel2302 <gabriel23002@gmail.com>
2020-06-29 03:14:05 -0300

    Toast Component

---
+++ gobarber-web/src/hooks/auth.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import React, { createContext, useCallback, useState, useContext } from 'react';
+
+import api from '../services/api';
+
+interface SignCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentials: SignCredentials): Promise<void>;
+  signOut(): void;
+}
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@Gobarber:token');
+    const user = localStorage.getItem('@Gobarber:token');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('/sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@Gobarber:token', token);
+    localStorage.setItem('@Gobarber:token', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@Gobarber:token');
+    localStorage.removeItem('@Gobarber:token');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
