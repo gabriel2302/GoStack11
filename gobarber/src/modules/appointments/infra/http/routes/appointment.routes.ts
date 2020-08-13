e60ecd5900959a763c5d9731d936a7f37642b9fb
gabriel2302 <gabriel23002@gmail.com>
2020-08-13 18:43:20 -0300

    Alteração no modulo User

--- gobarber/src/modules/appointments/infra/http/routes/appointment.routes.ts
+++ gobarber/src/modules/appointments/infra/http/routes/appointment.routes.ts
@@ -7,7 +7,6 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
-const appoinmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -21,6 +20,7 @@ appointmentsRouter.use(ensureAuthenticated);
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
+  const appoinmentsRepository = new AppointmentsRepository();
   const parsedDate = parseISO(date);
   const createAppointment = new CreateAppointmentService(appoinmentsRepository);
 
