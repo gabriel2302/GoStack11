5ff0c3773210f6c6ae22c80cb6e3edeab3fa0d3d
gabriel2302 <gabriel23002@gmail.com>
2020-09-02 06:38:04 -0300

    listagem de prestadores

--- gobarber/src/shared/infra/http/routes/index.ts
+++ gobarber/src/shared/infra/http/routes/index.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 
 import appointmentRouter from '@modules/appointments/infra/http/routes/appointment.routes';
-import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
-import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
