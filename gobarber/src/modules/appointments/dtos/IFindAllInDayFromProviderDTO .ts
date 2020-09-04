6f1077c66a422f447be46f9afed56d1f00dd95f4
gabriel2302 <gabriel23002@gmail.com>
2020-09-04 00:37:46 -0300

    agendamentos

---
+++ gobarber/src/modules/appointments/dtos/IFindAllInDayFromProviderDTO .ts
@@ -0,0 +1,6 @@
+export default interface IFindAllIntMonthFromProviderDTO {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
---
+++ gobarber/src/modules/appointments/dtos/IFindAllInMonthFromProviderDTO.ts
@@ -0,0 +1,5 @@
+export default interface IFindAllIntMonthFromProviderDTO {
+  provider_id: string;
+  month: number;
+  year: number;
+}
