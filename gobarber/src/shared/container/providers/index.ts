20ab3b78fdc0c717824461042d666f1d5b643240
gabriel2302 <gabriel23002@gmail.com>
2020-09-14 23:21:18 -0300

    AWS

--- gobarber/src/shared/container/providers/index.ts
+++ gobarber/src/shared/container/providers/index.ts
@@ -1,6 +1,10 @@
 import { container } from 'tsyringe';
 
+import mailConfig from '@config/mail';
+
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
+
 import IMailProvider from './MailProvider/models/IMailProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
@@ -19,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
